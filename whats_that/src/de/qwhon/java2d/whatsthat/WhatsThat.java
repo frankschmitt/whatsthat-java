e30e466e0a26fb704482e6c60fc04bb74556ab57
Frank Schmitt <frankschmitt@gmx.de>
2010-08-31 22:33:54 +0200

    WhatsThat: turning of tiles by button click WhatsThat: image is now scaled

--- whats_that/src/de/qwhon/java2d/whatsthat/WhatsThat.java
+++ whats_that/src/de/qwhon/java2d/whatsthat/WhatsThat.java
@@ -6,98 +6,145 @@ import java.awt.image.*;
 import java.awt.geom.*;
 import java.io.*;
 import javax.imageio.*;
-import javax.swing.*;
+//import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+//import javax.swing.SwingUtilities;
+
 
 
 /**
  * This class demonstrates how to load an Image from an external file
  */
 public class WhatsThat extends Component {
-          
-    final static Color black = Color.black;
-    final static Color red = Color.red;
-    final static Color white = Color.white;
 
-    static final int NumRows = 10;
-    static final int NumCols = 10;
-    
-    BufferedImage img;
+	private static final long serialVersionUID = 1L;
+	private int NumVisibleTiles = 0;
 
-    private void paintRectangle(Graphics2D g2, double x, double y, double width, double height, Color col, boolean fill) {
-        if (fill) {	g2.setPaint(col);
-            g2.fill(new Rectangle2D.Double(x, y,
-	                width,
-	                height));
-        }
-        else {
-        g2.draw(new Rectangle2D.Double(x, y,
-        	                width,
-                height));
-        }
-    }
+	final static Color black = Color.black;
+	final static Color red = Color.red;
+	final static Color white = Color.white;
 
-    
-    public void paint(Graphics g) {
-        g.drawImage(img, 0, 0, null);
+	static final int NumRows = 10;
+	static final int NumCols = 10;
 
-     // draw Rectangle2D.Double
-        double img_height = img.getHeight();
-        double img_width = img.getWidth();
-        double x = 0;
-        double y = 0;
-        
-        double tile_width = img_width / (double) NumCols;
-        double tile_height = img_height / (double) NumRows;
-        Color col;
-        boolean fill;
-    	Graphics2D g2 = (Graphics2D) g;
-        for (int i=0; i<NumRows; ++i) {
-        	x = tile_width*i;
-        	for (int j=0; j<NumCols; ++j) {
-        		y = tile_height*j;
-        	if ((i%2 + j%2)%2 == 0) {
-        		col = white;
-        		fill = true;
-        	}
-        	else {
-        		col = black;
-        		fill = false;
-        	}
-        	   paintRectangle(g2, x, y, tile_width, tile_height, col, fill);
-        	}
-        }
-     }
+	BufferedImage img;
+	JButton button;
 
-    public WhatsThat() {
-       try {
-           img = ImageIO.read(new File("data/DSC00390.JPG"));
-           //img = ImageIO.read(new File("data/strawberry.jpg"));
-           } catch (IOException e) {
-       }
+	private void paintRectangle(Graphics2D g2, double x, double y, double width, double height, Color col, boolean fill) {
+		if (fill) {	g2.setPaint(col);
+		g2.fill(new Rectangle2D.Double(x, y,
+				width,
+				height));
+		}
+		else {
+			g2.draw(new Rectangle2D.Double(x, y,
+					width,
+					height));
+		}
+	}
 
-    }
 
-    public Dimension getPreferredSize() {
-        if (img == null) {
-             return new Dimension(100,100);
-        } else {
-           return new Dimension(img.getWidth(null), img.getHeight(null));
-       }
-    }
+	public void paint(Graphics g) {
+		// draw whole image 1:1
+		//g.drawImage(img, 0, 0, null);
+		// scale image
+		g.drawImage(img,
+                0, 0, getWidth(), getHeight(), 
+                0, 0, img.getWidth(), img.getHeight(), null);
 
-    public static void main(String[] args) {
 
-        JFrame f = new JFrame("Load Image Sample");
-            
-        f.addWindowListener(new WindowAdapter(){
-                public void windowClosing(WindowEvent e) {
-                    System.exit(0);
-                }
-            });
+		// draw Rectangle2D.Double
+		double img_height = img.getHeight();
+		double img_width = img.getWidth();
+		double x = 0;
+		double y = 0;
 
-        f.add(new WhatsThat());
-        f.pack();
-        f.setVisible(true);
-    }
+		double tile_width = img_width / (double) NumCols;
+		double tile_height = img_height / (double) NumRows;
+		Color col;
+		boolean fill;
+		Graphics2D g2 = (Graphics2D) g;
+		for (int i=0; i<NumRows; ++i) {
+			x = tile_width*i;
+			for (int j=0; j<NumCols; ++j) {
+				// determine whether tile should be visible or not
+				//fill = (i > NumVisibleTiles);
+				fill = ((i+j*NumRows) > NumVisibleTiles);
+				y = tile_height*j;
+				if ((i%2 + j%2)%2 == 0) {
+					col = white;
+					//fill = true;
+				}
+				else {
+					col = black;
+					//fill = false;
+				}
+				paintRectangle(g2, x, y, tile_width, tile_height, col, fill);
+			}
+		}
+	}
+
+	public void buttonClicked(ActionEvent e) {
+		//button.setEnabled(!button.isEnabled());
+		++NumVisibleTiles;
+		repaint();
+	}
+
+	private void initUI(JFrame f) {
+		//setSize(200,00);
+		//setLayout(new BorderLayout());
+
+		JToolBar toolbar = new JToolBar();
+		button= new JButton("Show next tile");
+		//button.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) { button.setEnabled(false);}});
+		button.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) { buttonClicked(e); }});
+		toolbar.add(button);
+		f.getContentPane().add(toolbar, BorderLayout.NORTH);
+	}
+
+	public WhatsThat() {
+		try {
+			img = ImageIO.read(new File("data/DSC00390.JPG"));
+			//img = ImageIO.read(new File("data/strawberry.jpg"));
+		} catch (IOException e) {
+		}
+
+	}
+
+	public Dimension getPreferredSize() {
+		if (img == null) {
+			return new Dimension(100,100);
+		} else {
+			return new Dimension(1024, 768);
+			//return new Dimension(img.getWidth(null), img.getHeight(null));
+		}
+	}
+
+	public static void main(String[] args) {
+
+		JFrame f = new JFrame("Load Image Sample");
+
+		f.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		WhatsThat w = new WhatsThat();
+		w.initUI(f);
+		f.add(w);
+		f.pack();
+		f.setVisible(true);
+	}
 }
 
